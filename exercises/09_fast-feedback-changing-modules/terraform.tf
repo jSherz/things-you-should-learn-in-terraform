8ac5c8e617d4ef9d1624bf568777b8938d2c981d
James Sherwood-Jones <james@jsherz.com>
2023-07-14 07:57:14 +0100

    Fix example.

--- exercises/09_fast-feedback-changing-modules/terraform.tf
+++ exercises/09_fast-feedback-changing-modules/terraform.tf
@@ -2,11 +2,9 @@ terraform {
   required_version = "~> 1.5"
 
   backend "s3" {
-    bucket         = "my-tf-state-247940857651-eu-west-1" # "my-tf-state-<account ID>-<region>"
-    region         = "eu-west-1" # "<region>"
-#    bucket         = "my-tf-state-<account ID>-<region>"
+    bucket         = "my-tf-state-<account ID>-<region>"
     key            = "things-you-should-learn-in-terraform/09_fast-feedback-changing-modules/terraform.tfstate"
-#    region         = "<region>"
+    region         = "<region>"
     dynamodb_table = "my-tf-state-locks"
   }
 
